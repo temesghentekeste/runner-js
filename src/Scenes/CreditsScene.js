2489604c0f0180f74cd4ae751c0c1a05583d471b
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 12:28:27 +0300

    Add CreditsScene.js

---
+++ src/Scenes/CreditsScene.js
@@ -0,0 +1,21 @@
+4;
+5;
+6;
+7;
+8;
+9;
+10;
+11;
+12;
+13;
+import 'phaser';
+
+export default class CreditsScene extends Phaser.Scene {
+  constructor() {
+    super('Credits');
+  }
+
+  preload() {}
+
+  create() {}
+}

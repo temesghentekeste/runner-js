3c30957f4c6d6cee8f6a3a23b2e31a3b76f68852
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 13:57:56 +0300

    Add play game button  sprite  - Make it intereactive  - Center the button and the text

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -7,5 +7,30 @@ export default class TitleScene extends Phaser.Scene {
 
   preload() {}
 
-  create() {}
+  create() {
+    // Game
+    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.gameButton, 1);
+
+    this.gameText = this.add.text(0, 0, 'Play', {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+    this.centerButtonText(this.gameText, this.gameButton);
+
+    this.gameButton.on(
+      'pointerdown',
+      function (pointer) {
+        this.scene.start('Game');
+      }.bind(this)
+    );
+
+    this.input.on('pointerover', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton2');
+    });
+
+    this.input.on('pointerout', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton1');
+    });
+  }
 }

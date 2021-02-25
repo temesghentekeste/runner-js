99229c47dc34c66f2a3c34c340caeba166e21ddc
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 14:00:59 +0300

    Add centerButton and centerButtonText methods

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -33,4 +33,20 @@ export default class TitleScene extends Phaser.Scene {
       gameObjects[0].setTexture('blueButton1');
     });
   }
+
+  centerButton(gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(
+        config.width / 2,
+        config.height / 2 - offset * 100,
+        config.width,
+        config.height
+      )
+    );
+  }
+
+  centerButtonText(gameText, gameButton) {
+    Phaser.Display.Align.In.Center(gameText, gameButton);
+  }
 }

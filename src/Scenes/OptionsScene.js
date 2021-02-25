a2bf6dee9b35c6377c8cb0f976618aa77b03d77e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 12:27:10 +0300

    Add OptionsScene.js

---
+++ src/Scenes/OptionsScene.js
@@ -0,0 +1,22 @@
+3;
+4;
+5;
+6;
+7;
+8;
+9;
+10;
+11;
+12;
+13;
+import 'phaser';
+
+export default class OptionsScene extends Phaser.Scene {
+  constructor() {
+    super('Options');
+  }
+
+  preload() {}
+
+  create() {}
+}

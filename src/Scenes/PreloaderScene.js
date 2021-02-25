5651ffa612a36a00cb081b60ea8b0514f2cff4ae
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 13:05:17 +0300

    Prepare PreloadScene to transitionto the Title Scene  - Update the function that is ran when the complete event is fired to call the new ready function  - Add a new timed event that calls the new ready function when triggered after 3 sec.  - Add Phasers Scene’s init function that is triggered when a scene first starts  - Create a new variable called readyCount and  set it to 0.  - Add a new function called ready, and when called  increment the readyCount variable.  - Transition to the Title Scene when the value of the varialbe is 2.

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -64,19 +64,36 @@ export default class PreloaderScene extends Phaser.Scene {
     });
 
     // remove progress bar when complete
-    this.load.on('complete', function () {
-      progressBar.destroy();
-      progressBox.destroy();
-      loadingText.destroy();
-      percentText.destroy();
-      assetText.destroy();
-    });
+    this.load.on(
+      'complete',
+      function () {
+        progressBar.destroy();
+        progressBox.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+        assetText.destroy();
+        this.ready();
+      }.bind(this)
+    );
+
+    this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
     // load assets needed in our game
-    this.load.image('blueButton1', 'assets/ui/blue_button02.png');
-    this.load.image('blueButton2', 'assets/ui/blue_button03.png');
-    this.load.image('phaserLogo', 'assets/logo.png');
+    this.load.image('blueButton1', 'src/assets/ui/blue_button02.png');
+    this.load.image('blueButton2', 'src/assets/ui/blue_button03.png');
+    this.load.image('phaserLogo', 'src/assets/logo.png');
   }
 
   create() {}
+
+  init() {
+    this.readyCount = 0;
+  }
+
+  ready() {
+    this.readyCount++;
+    if (this.readyCount === 2) {
+      this.scene.start('Title');
+    }
+  }
 }

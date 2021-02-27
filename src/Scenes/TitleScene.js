59480e3b8ece63495d4a23968df24513cb8f1f8d
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-27 19:09:44 +0300

    Add credits button inside TitleScene's create method

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -52,6 +52,33 @@ export default class TitleScene extends Phaser.Scene {
     this.input.on('pointerout', function (event, gameObjects) {
       gameObjects[0].setTexture('blueButton1');
     });
+
+    // Credits
+    this.creditsButton = this.add
+      .sprite(300, 200, 'blueButton1')
+      .setInteractive();
+    this.centerButton(this.creditsButton, -1);
+
+    this.creditsText = this.add.text(0, 0, 'Credits', {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+    this.centerButtonText(this.creditsText, this.creditsButton);
+
+    this.creditsButton.on(
+      'pointerdown',
+      function (pointer) {
+        this.scene.start('Credits');
+      }.bind(this)
+    );
+
+    this.input.on('pointerover', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton2');
+    });
+
+    this.input.on('pointerout', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton1');
+    });
   }
 
   centerButton(gameObject, offset = 0) {

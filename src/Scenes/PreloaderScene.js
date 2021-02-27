451064743c1eb1337d4642651608c42fecdb02c7
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-27 18:39:19 +0300

    Update the loading progress bar

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -6,20 +6,17 @@ export default class PreloaderScene extends Phaser.Scene {
   }
 
   preload() {
-
-    console.log('Preload running');
     // add logo image
     this.add.image(400, 200, 'logo');
 
     // display progress bar
-    var progressBar = this.add.graphics();
-    var progressBox = this.add.graphics();
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
     progressBox.fillRect(240, 270, 320, 50);
-
-    var width = this.cameras.main.width;
-    var height = this.cameras.main.height;
-    var loadingText = this.make.text({
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
       x: width / 2,
       y: height / 2 - 50,
       text: 'Loading...',
@@ -28,9 +25,9 @@ export default class PreloaderScene extends Phaser.Scene {
         fill: '#ffffff',
       },
     });
-    loadingText.setOrigin(0.5, 0.5);
+    loadingText.setOrigin(1.5, 0.5);
 
-    var percentText = this.make.text({
+    const percentText = this.make.text({
       x: width / 2,
       y: height / 2 - 5,
       text: '0%',
@@ -41,7 +38,7 @@ export default class PreloaderScene extends Phaser.Scene {
     });
     percentText.setOrigin(0.5, 0.5);
 
-    var assetText = this.make.text({
+    const assetText = this.make.text({
       x: width / 2,
       y: height / 2 + 50,
       text: '',
@@ -53,30 +50,26 @@ export default class PreloaderScene extends Phaser.Scene {
     assetText.setOrigin(0.5, 0.5);
 
     // update progress bar
-    this.load.on('progress', function (value) {
-      percentText.setText(parseInt(value * 100) + '%');
+    this.load.on('progress', (value) => {
+      // eslint-disable-next-line radix
+      percentText.setText(`${parseInt(value * 100)}%`);
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
       progressBar.fillRect(250, 280, 300 * value, 30);
     });
 
-    // update file progress text
-    this.load.on('fileprogress', function (file) {
-      assetText.setText('Loading asset: ' + file.key);
+    this.load.on('fileprogress', (file) => {
+      assetText.setText(`Loading asset: ${file.key}`);
     });
 
-    // remove progress bar when complete
-    this.load.on(
-      'complete',
-      function () {
-        progressBar.destroy();
-        progressBox.destroy();
-        loadingText.destroy();
-        percentText.destroy();
-        assetText.destroy();
-        this.ready();
-      }.bind(this)
-    );
+    this.load.on('complete', () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+      this.ready();
+    });
 
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 

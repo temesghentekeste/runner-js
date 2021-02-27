1a94a9a7d66778ca8992f8d88a6347f680874b85
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-28 01:19:00 +0300

    Add global game options

---
+++ src/Config/gameState.js
@@ -0,0 +1,12 @@
+// global game options
+let gameOptions = {
+  platformStartSpeed: 350,
+  spawnRange: [100, 350],
+  platformSizeRange: [50, 250],
+  playerGravity: 900,
+  jumpForce: 400,
+  playerStartPosition: 200,
+  jumps: 2,
+};
+
+export default gameOptions;

33d7fa104764e9169b81f5022494548ee33a287f
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-27 21:24:13 +0300

    Refactor code inside Options scene and replace navigate to Title button with Button component

--- src/Scenes/OptionsScene.js
+++ src/Scenes/OptionsScene.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import Button from '../Components/Button'
 
 export default class OptionsScene extends Phaser.Scene {
   constructor() {
@@ -38,18 +39,14 @@ export default class OptionsScene extends Phaser.Scene {
     );
 
     // Navigate back to the TitleScene
-    this.menuButton = this.add.sprite(400, 500, 'blueButton1').setInteractive();
-    this.menuText = this.add.text(0, 0, 'Menu', {
-      fontSize: '32px',
-      fill: '#fff',
-    });
-    Phaser.Display.Align.In.Center(this.menuText, this.menuButton);
-
-    this.menuButton.on(
-      'pointerdown',
-      function (pointer) {
-        this.scene.start('Title');
-      }.bind(this)
+    this.menuButton = new Button(
+      this,
+      400,
+      500,
+      'blueButton1',
+      'blueButton2',
+      'Menu',
+      'Title'
     );
 
     this.updateAudio();

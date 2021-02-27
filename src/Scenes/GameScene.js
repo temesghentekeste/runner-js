b1fda9dd04e94b121a666e54e3858a68f3533195
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-28 01:26:14 +0300

    Create platfomrs on the fly or add from pool; group platforms; set jumps made by player

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import gameState from '../Config/gameState';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -18,6 +19,52 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(400, 300, 'logo');
+    // group with all active platforms.
+    this.platformGroup = this.add.group({
+      // once a platform is removed, it's added to the pool
+      removeCallback: function (platform) {
+        platform.scene.platformPool.add(platform);
+      },
+    });
+
+    // pool
+    this.platformPool = this.add.group({
+      // once a platform is removed from the pool, it's added to the active platforms group
+      removeCallback: function (platform) {
+        platform.scene.platformGroup.add(platform);
+      },
+    });
+
+    // number of consecutive jumps made by the player
+    this.playerJumps = 0;
+
+    // adding a platform to the game, the arguments are platform width and x position
+    this.addPlatform(game.config.width, game.config.width / 2);
+  }
+
+  // the core of the script: platform are added from the pool or created on the fly
+  addPlatform(platformWidth, posX) {
+    let platform;
+    if (this.platformPool.getLength()) {
+      platform = this.platformPool.getFirst();
+      platform.x = posX;
+      platform.active = true;
+      platform.visible = true;
+      this.platformPool.remove(platform);
+    } else {
+      platform = this.physics.add.sprite(
+        posX,
+        game.config.height * 0.8,
+        'platform'
+      );
+      platform.setImmovable(true);
+      platform.setVelocityX(gameState.platformStartSpeed * -1);
+      this.platformGroup.add(platform);
+    }
+    platform.displayWidth = platformWidth;
+    this.nextPlatformDistance = Phaser.Math.Between(
+      gameState.spawnRange[0],
+      gameState.spawnRange[1]
+    );
   }
 }

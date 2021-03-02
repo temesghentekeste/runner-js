f4edc6e88a7760711929031d8fa18e0b31b5f383
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-03-02 12:34:23 +0300

    Add test for GameOverScene

---
+++ src/__tests__/GameOverScene.test.js
@@ -0,0 +1,13 @@
+import GameOverScene from '../Scenes/GameOverScene';
+
+const gameOverScene = new GameOverScene();
+
+test('gameOverScene is of type object', () => {
+  expect(typeof gameOverScene).toBe('object');
+});
+
+test('gameOverScene key name is Boot', () => {
+  expect(gameOverScene.sys.config).toBe('GameOver');
+});
+
+
